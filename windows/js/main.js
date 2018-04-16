a3864e024f3d874398d07ab652dfb01b14ea8ca9
Ann <34541660+ilomilomimi@users.noreply.github.com>
2018-04-16 08:29:12 +0300

    Add files via upload

--- windows/js/main.js
+++ windows/js/main.js
@@ -2,50 +2,146 @@ $(document).ready(function(){
     $('.slider-price__slider').slick({
         infinite: true,
         slidesToShow: 4,
-        slidesToScroll: 4,
+        slidesToScroll: 1,
+
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 567,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 320,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
+    });
+
+    $('.gallery__slider').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
 
         responsive: [{
         }, {
-            breakpoint: 769,
+            breakpoint: 1200,
             settings: {
-                slidesToShow: 1,
+                slidesToShow: 2,
                 slidesToScroll: 1
             }
 
         }, {
-            breakpoint: 300,
-            settings: "unslick" // destroys slick
+            breakpoint: 768,
 
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false
+            }
+
+        }, {
+            breakpoint: 320,
+            settings: {
+                arrows: false
+            }
         }]
     });
-    $('.gallery__slider').slick({
-        infinite: true
-    });
+
     $('.gallery__slider-small').slick({
-        infinite: true
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+
+        responsive: [{
+        }, {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+
+        }, {
+            breakpoint: 768,
+
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false
+            }
+        }, {
+            breakpoint: 320,
+            settings: {
+                arrows: false
+            }
+        }]
+
     });
+
     $('.reviews__slider').slick({
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 2,
+        slidesToScroll: 1,
 
         responsive: [{
             }, {
-                breakpoint: 769,
+                breakpoint: 1200,
                 settings: {
-                    slidesToShow: 1,
+                    slidesToShow: 2,
                     slidesToScroll: 1
                 }
 
             }, {
-                breakpoint: 300,
-                settings: "unslick" // destroys slick
-
-            }]
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false
+            }
+        }, {
+            breakpoint: 320,
+            settings: {
+                arrows: false
+            }
+        }]
     });
+
+
     $("#icons-plus .tab-info__icon").click(function(e) {
         e.preventDefault();
         $("#icons-plus .tab-info__icon").removeClass('icon-plus--active');
-        $(this).addClass('icon-plus--active');
+        $(this).toggleClass('icon-plus--active');
     });
+
+    $('.tab-info__icon--1').popover(
+        {content: "<div class='tab-tooltip__icon'><img src='img/logo_kbe.png'></div> <div class='tab-tooltip__text'>пластиковый профиль KBE (Германия) шириной 58 мм, 70 мм или 127 мм (на выбор)</div> ", html: true, placement: "left"}
+        );
+    $('.tab-info__icon--2').popover(
+        {content: "<div class='tab-tooltip__icon'><img src='img/logo_kbe.png'></div> <div class='tab-tooltip__text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aspernatur at, debitis doloribus est facere impedit maxime natus numquam possimus quod quos rerum tempore voluptatem voluptates! Eveniet facilis quisquam voluptatibus.</div> ", html: true, placement: "right"}
+        );
+    $('.tab-info__icon--3').popover(
+        {content: "<div class='tab-tooltip__icon'><img src='img/logo_kbe.png'></div> <div class='tab-tooltip__text'>test 3</div> ", html: true, placement: "left"}
+        );
+    $('.tab-info__icon--4').popover(
+        {content: "<div class='tab-tooltip__icon'><img src='img/logo_kbe.png'></div> <div class='tab-tooltip__text'>test 4</div> ", html: true, placement: "right"}
+        );
+
 });
