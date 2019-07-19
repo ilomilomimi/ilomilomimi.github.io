47e8c8ecba1bcd19328d8e5cd374cc30d9421d72
Ann <34541660+ilomilomimi@users.noreply.github.com>
2019-07-19 20:35:11 +0300

    Add files via upload

--- astoni/src/js/main.js
+++ astoni/src/js/main.js
@@ -16,3 +16,42 @@ $('.menu-icon').click(function () {
     $(".menu-icon").toggleClass('open');
     $("body").toggleClass('hidden');
 });
+
+// json
+var data = {
+    "links": [
+        {
+            "name": "Home",
+            "url": "#"
+        },
+        {
+            "name": "About",
+            "url": "\/about"
+        },
+        {
+            "name": "Pricing",
+            "url": "\/pricing"
+        },
+        {
+            "name": "Our drones",
+            "url": "\/our-drones"
+        },
+        {
+            "name": "Our realizations",
+            "url": "\/our-realizations"
+        },
+        {
+            "name": "Contact",
+            "url": "http:\/\/ya.ru",
+            "marked": true
+        }
+    ]
+};
+$(data.links).each(function(index, item) {
+    if (item.marked == true) {
+        $('#header-menu').append('<li class="menu__item"><a href="'+ item.url +'" class="menu__btn" target="_blank">' + item.name + '</a></li>');
+    }
+    else {
+        $('#header-menu').append('<li class="menu__item"><a href="'+ item.url +'" class="menu__link">' + item.name + '</a></li>');
+    }
+});
